848f0a08083033602ab09311daa81d75ee056eae
Pablo Zumarán <pablozumaran@inacapmail.cl>
2022-05-12 18:05:22 -0400

    actualizacion2.0 12-05

---
+++ intento de formulario clase 06-05/holamundo.js
@@ -0,0 +1,37 @@
+// crear una alerta en el archivo html
+
+alert("Bienvenido Le solicitaremos sus datos personales");
+console.log("Bienvenido Le solicitaremos sus datos personales");
+
+// 
+let variable = 45;
+const constante = 10;
+console.log(variable,constante);
+
+// funcion
+
+function funcion(){
+
+   let obj = document.getElementById("edad");
+
+   console.log(obj.value);
+
+
+    obj.value >= 18 ? document.getElementById("aviso").value="usted es mayor" :  document.getElementById ("aviso").value="usted es menor de edad"
+
+
+
+    let arreglo = []
+
+    for (let i = 0; i <4 ; i++ ){
+        
+    }
+
+    let genero = document.getElementById
+    ("sexo")
+    console.log(genero.value)
+
+    for(let x = 0; x < arreglo.length; x++){
+        console.log(arreglo[x])
+    }
+}
